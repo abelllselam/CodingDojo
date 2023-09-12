72731c1fcac730833d841504b272d074f570056d
Abel Woldemichael <abelllselam@gmail.com>
2023-09-12 11:08:33 -0400

    new algo

--- Python/flask/checkerboard/server.py
+++ Python/flask/checkerboard/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import math
 
 app = Flask(__name__)
 
@@ -15,7 +16,13 @@ def second():
 
 @app.route("/<int:x>/<int:y>")
 def x_and_y(x, y):
-    return render_template("x_and_y.html", x=x, y=y)
+    num_x = x - (x / 2)
+    math.floor(int(num_x))
+    print(num_x)
+    num_y = y - (y / 2)
+    math.floor(num_y)
+
+    return render_template("x_and_y.html", x=x, y=y, num_x=num_x, num_y=num_y)
 
 
 if __name__ == "__main__":
