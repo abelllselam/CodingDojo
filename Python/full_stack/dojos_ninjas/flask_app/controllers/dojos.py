2e1737ac8ec22d8c57801470c3ef903df8ffbde3
Abel Woldemichael <abelllselam@gmail.com>
2023-09-19 20:10:54 -0400

    new_apps

--- Python/full_stack/dojos_ninjas/flask_app/controllers/dojos.py
+++ Python/full_stack/dojos_ninjas/flask_app/controllers/dojos.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, redirect, request, session
-
+from flask_app.models import dojo, ninja
 from flask_app import app
 
-from flask_app.models.dojo import Dojo
 
-
-@app.route("/")
+@app.route("/dojos")
 def index():
-    return render_template("index.html")
+    dojos = dojo.Dojo.get_all()
+    return render_template("dojo.html", dojos=dojos)
+
+
+@app.post("/dojos/create")
+def create():
+    print("Form Data ---------------------------------> ", request.form)
+    dojos = dojo.Dojo.create(request.form)
+
+    return redirect("/dojos")
+
+
+@app.route("/show/<int:id>")
+def Show_all(id):
+    data = {"id": id}
+    # get data of dojo and ninjas associated with it. one to many!
+    dojos_info = dojo.Dojo.dojos_and_ninjas(data)
+    return render_template("show.html", dojos_info=dojos_info)
--- Python/full_stack/dojos_ninjas/server.py
+++ Python/full_stack/dojos_ninjas/server.py
@@ -1,4 +1,6 @@
-from flask_app.controllers import dojos, ninjas
+from flask_app.controllers import dojos
+from flask_app.controllers import ninjas
+
 
 from flask_app import app
 
